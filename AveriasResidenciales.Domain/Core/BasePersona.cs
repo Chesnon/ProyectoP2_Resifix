70f673d9e0b0c041abf014fe2298a1bc8f7d8450
Chesnon <jeremyalexander135@gmail.com>
2026-08-18 01:30:22 -0400

    Agregando Capa Domain
    N/A

---
+++ AveriasResidenciales.Domain/Core/BasePersona.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AveriasResidenciales.Domain.Core
+{
+    public class BasePersona
+    {
+        public int id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ AveriasResidenciales.Domain/Enums/PrioridadAveria.cs
@@ -0,0 +1,10 @@
+namespace AveriasResidenciales.Domain.Enums
+{
+    public enum PrioridadAveria
+    {
+        Baja = 0,
+        Media = 1,
+        Alta = 2,
+        Urgente = 3
+    }
+}
